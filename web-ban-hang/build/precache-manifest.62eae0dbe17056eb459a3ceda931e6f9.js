bf714f5b5ca2d485051e455803b4e7bf6615300b
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-07-30 09:04:03 +0700

    create Project-01-myWeb-Reactjs
    create Project-01-myWeb-Reactjs

---
+++ web-ban-hang/build/precache-manifest.62eae0dbe17056eb459a3ceda931e6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ca33a22876a6985a8f23c67ddf241d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1843f640b7dd8ae5e78a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1843f640b7dd8ae5e78a",
+    "url": "/static/js/main.cd6e0384.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
